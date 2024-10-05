327b03ed080587d8d5995a3025c73ce4334c2c85
David E O <davideoguche@gmail.com>
2024-10-05 06:23:06 +0100

    module and function example

---
+++ 1.modules.py
@@ -0,0 +1,8 @@
+import re
+my_regex = re.compile("[0-9]+", re.I)
+
+import matplotlib.pyplot as plt
+
+from collections import defaultdict, Counter
+lookup = defaultdict(int)
+my_counter = Counter()
