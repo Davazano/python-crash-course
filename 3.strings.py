6957b181357c7ad0407243d44180466b9f4f891a
David E O <davideoguche@gmail.com>
2024-10-05 06:30:52 +0100

    added strings file

---
+++ 3.strings.py
@@ -0,0 +1,14 @@
+single_qouted_string = 'data science'
+double_qouted_string = "data science"
+
+tab_string = "\t"
+len(tab_string)
+
+# raw string
+not_tab_string = r"\t"
+len(not_tab_string)
+
+# multiline string
+multi_line_string = """This is the first line.
+and this is the second line
+and this is the third line"""
